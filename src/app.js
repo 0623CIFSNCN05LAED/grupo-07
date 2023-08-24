708fac7a3c7239e8698672d357a912f586bd9c85
FelipeB21 <bolgarfelipe@gmail.com>
2023-08-24 03:42:16 -0300

    login

--- src/app.js
+++ src/app.js
@@ -14,3 +14,7 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views/productCart.html"))
 })
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/login.html"))
+})
