d1dc66e341fd7ade01a83f18b3c90fd0ab0f21a1
JonathanUGomez93 <jonathanurielgomez1993@gmail.com>
2023-10-01 18:55:12 -0300

    comienzo a trabajar user-guard

--- src/routes/main-router.js
+++ src/routes/main-router.js
@@ -3,11 +3,10 @@ const { Router } = require("express");
 const router = Router();
 
 const mainController = require('../controllers/main-controller')
-const productController = require('../controllers/ProductController')
+
 router.get("/cart", mainController.cart);
 router.get("/productlist", mainController.list);
 router.get("/login", mainController.login);
-router.get("/formproduct", mainController.create);
-router.post("/createproduct", productController.create)
-router.get("/usersData",usersController.users);
+router.get("/createproduct", mainController.create);
+
 module.exports = router;
