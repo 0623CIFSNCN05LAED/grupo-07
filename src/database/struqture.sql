7df7865c6c10a123edef9a7ede1830eee83cee2d
JonathanUGomez93 <jonathanurielgomez1993@gmail.com>
2023-11-20 21:59:11 -0300

    trabajo modelo products

--- src/database/struqture.sql
+++ src/database/struqture.sql
@@ -1,54 +1,51 @@
-DROP TABLE IF EXISTS (MercadoArte);
+DROP DATABASE IF EXISTS mercadoarte;
 
-USE MercadoArte;
+CREATE DATABASE mercadoarte;
+USE mercadoarte;
 
-CREATE TABLE (technique);   (
-    id INT PRIMARY KEY,
-    type VARCHAR(255) NOT NULL,
-
-    
+CREATE TABLE techniques  (
+    id VARCHAR(255) PRIMARY KEY,
+    type VARCHAR(255) NOT NULL
 );
 
 
-CREATE TABLE (artists); (
-    id INT PRIMARY KEY,
+CREATE TABLE artists (
+    id VARCHAR(255) PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    nacionality VARCHAR(255) NOT NULL,
+    nacionality VARCHAR(255) NOT NULL
 );
 
 
-CREATE TABLE (clients); (
-    id PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255),
-   address VARCHAR(255),
-);
-
-
-CREATE TABLE (sales); (
-    id INT PRIMARY KEY,
-    sales_date DATE NOT NULL,
-    FOREIGN KEY (product_id) REFERENCES products (id),
-    FOREIGN KEY (client_id) REFERENCES clients (id),
+CREATE TABLE clients (
+id VARCHAR(255) PRIMARY key ,
+name VARCHAR(255),
+email VARCHAR(255),
+address VARCHAR(255)
 );
 
 
 
-
-CREATE TABLE (products);  (
-    id INT PRIMARY KEY,
+CREATE TABLE products  (
+    id VARCHAR(255) PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    creation_date DATE NOT NULL,
+    creation_date VARCHAR(255) NOT NULL,
     description TEXT,
     price DECIMAL(10, 2) NOT NULL,
-    on_sale BOOLEAN,
-    new BOOLEAN,
+    on_sale TINYINT(1),
+    new TINYINT(1),
     img VARCHAR(255),
-    technique_id INT,
-    artist_id INT,
-    FOREIGN KEY (technique_id) REFERENCES Techniques(id),
-    FOREIGN KEY (artist_id) REFERENCES Artists(id),
+    technique_id VARCHAR(255),
+    artist_id VARCHAR(255),
+    FOREIGN KEY (technique_id) REFERENCES techniques(id),
+    FOREIGN KEY (artist_id) REFERENCES artists(id)
 );
 
-
+CREATE TABLE sales (
+    id VARCHAR(255) PRIMARY KEY,
+    sales_date VARCHAR(255) NOT NULL,
+    product_id VARCHAR(255),
+    client_id VARCHAR(255),
+    FOREIGN KEY (product_id) REFERENCES products (id),
+    FOREIGN KEY (client_id) REFERENCES clients (id)
+);
